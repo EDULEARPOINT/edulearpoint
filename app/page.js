029a79c9bb907ee7b7b807f97c5be16efeefe381
edulearpoint@gmail.com <shyammishra5857@gmail.com>
2024-10-28 00:06:49 +0530

    minor changes

--- app/page.js
+++ app/page.js
@@ -1,23 +1,31 @@
-import Faq from "./_components/Faq";
-import Footer from "./_components/Footer";
-import Founder from "./_components/Founder";
-import Hero from "./_components/Hero";
-import JoiningPlatform from "./_components/JoiningPlatform";
-import Navbar from "./_components/Navbar";
-import OurTeam from "./_components/OurTeam";
-import Packages from "./_components/Packages";
-import VantaBackground from "./_components/VantaBackground";
+// import Faq from "./_components/Faq";
+// import Footer from "./_components/Footer";
+// import Founder from "./_components/Founder";
+// import Hero from "./_components/Hero";
+// import JoiningPlatform from "./_components/JoiningPlatform";
+// import Navbar from "./_components/Navbar";
+// import OurTeam from "./_components/OurTeam";
+// import Packages from "./_components/Packages";
+// import VantaBackground from "./_components/VantaBackground";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen w-screen overflow-hidden  flex-col">
-      <Hero />
-      <JoiningPlatform />
-      <Packages />
-      <Founder />
-      <OurTeam />
-      <Faq />
-      <Footer/>
-    </main>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen w-screen overflow-hidden  flex-col">
+//       <Hero />
+//       <JoiningPlatform />
+//       <Packages />
+//       <Founder />
+//       <OurTeam />
+//       <Faq />
+//       <Footer/>
+//     </main>
+//   );
+// }
+
+import React from "react";
+
+const page = () => {
+  return <div>🍾🍾this website is fake. 🥶🥶</div>;
+};
+
+export default page;
